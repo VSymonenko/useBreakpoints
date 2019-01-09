1bb12e006c391b800e23a775abbd880c0c589daa
VSymonenko <simonenko744@gmail.com>
2019-01-09 20:56:01 +0200

    update webpack dev server

---
+++ dev/build/precache-manifest.729b1ef870209d61b311fba83713f2f8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b6cd2798c94cc2834ae",
+    "url": "/static/js/main.0b6cd279.chunk.js"
+  },
+  {
+    "revision": "d91601375e976afe223b",
+    "url": "/static/js/1.d9160137.chunk.js"
+  },
+  {
+    "revision": "27b509ec65d1fef0e66df7af89316507",
+    "url": "/index.html"
+  }
+];
